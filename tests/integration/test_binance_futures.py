5ccb5cd695ede885e23a83fd7dd11ec41fba4bcb
lucka <lucka.work@outlook.com>
2023-04-07 13:04:59 +0200

    FEATURE: addeted another implementation to some of Binance tests.

--- tests/integration/test_binance_futures.py
+++ tests/integration/test_binance_futures.py
@@ -20,7 +20,7 @@ class BinanceFuturesTest(unittest.TestCase):
         )
 
         self.exchange.set_sandbox_mode(True)
-       # self.set_default_setting()
+    # self.set_default_setting()
 
         print("Finished SetUp")
 
@@ -28,27 +28,26 @@ class BinanceFuturesTest(unittest.TestCase):
     # def tearDown(self) -> None:
     #     print("Start Tear down")
     #
-    #     print("Cancel all derivatives orders")
-    #     orders = self.exchange.fetch_orders()
+    #     print("Cancel all orders")
+    #     orders = self.exchange.cancel_all_orders()
     #     for order in orders:
     #         order_info = order["info"]
     #         self.exchange.cancel_all_orders(symbol=order_info["symbol"])
     #
-    #     print("Cancel all derivatives positions")
+    #     print("Cancel all positions")
     #     positions = self.exchange.fetch_positions()
-    #     for position in positions:
-    #         position_info = position["info"]
-    #         self.exchange.create_order(symbol=position_info["symbol"],
-    #                                    type="market",
-    #                                    side="Sell" if position_info["side"] == "Buy" else "Buy",
-    #                                    amount=position_info["size"],
-    #                                    params={
-    #                                        "positionIdx": position_info["positionIdx"],
-    #                                        "reduceOnly": True})
+    #    for position in positions:
+    #             position_info = position["info"]
+    #             self.exchange.create_order(symbol=position_info["symbol"],
+    #                                        type="market",
+    #                                        side="short" if position["side"] == "long" else "long",
+    #                                        amount=position["side"],
+    #                                        params={
+    #                                            "reduceOnly": True})
 
        # self.set_default_setting()
 
-        print("Finished Tear down")
+        # print("Finished Tear down")
 
     def test_get_btc_current_price(self):
         print("Start test_get_btc_current_price")
@@ -71,7 +70,7 @@ class BinanceFuturesTest(unittest.TestCase):
 
         print("Finished test_get_btc_current_price")
 
-    def test_get_btc_daily_ohlc(self):
+    def test_get_btc_daily_ohlc(self):  # TODO: @lucka zkontrolovat metodu
         print("Start test_get_btc_daily_ohlc")
         url = "https://fapi.binance.com/fapi/v1/klines?symbol={}&interval={}"
 
@@ -81,7 +80,7 @@ class BinanceFuturesTest(unittest.TestCase):
         self.assertTrue(len(response) > 1)
         print("Finished test_get_btc_daily_ohlc")
 
-    def test_get_btcusdt_info(self):
+    def test_get_btcusdt_info(self):  # TODO: @lucka zkontrolovat metodu
         print("Start test_get_btcusdt_info")
         url = "https://fapi.binance.com/fapi/v1/exchangeInfo"
 
@@ -109,10 +108,96 @@ class BinanceFuturesTest(unittest.TestCase):
 
         print("Finished test_get_btcusdt_info")
 
-    def test_get_available_usdt_balance_on_account(self):
+    def test_get_available_usdt_balance_on_account(self):  # TODO: @lucka zkontrolovat metodu
         print("Start test_get_available_usdt_balance_on_account")
-        response = self.exchange.fetch_balance({"type": "future"})  # TODO: @lucka dokoncit metodu
+        response = self.exchange.fetch_balance({"type": "future"})
         print("Response: {}".format(response))
+
+    def test_buy_btc_by_market_order(self):  # TODO: @lucka dokoncit metodu zkontrolovat permisions for api
+        print("Start test_buy_btc_by_market_order")
+        response = self.exchange.create_order(symbol="BTC/USDT:USDT",
+                                              type="market",
+                                              side="buy",
+                                              amount=0.001
+                                              )
+
+        print("Response: {}".format(response))
+        self.assertTrue("id" in response)
+
+        print("Finished test_buy_btc_by_market_order")
+
+    def test_sell_btc_by_market_order(self):  # TODO: @lucka zkontrolovat metodu
+        print("Start test_sell_btc_by_market_order")
+        response = self.exchange.create_order(symbol="BTC/USDT:USDT",
+                                              type="market",
+                                              side="sell",
+                                              amount=0.001
+                                              )
+
+        print("Response: {}".format(response))
+        self.assertTrue("id" in response)
+        print("Finished test_sell_btc_by_market_order")
+
+    def test_buy_btc_by_limit_order(self):  # TODO: @lucka dokoncit metodu zkontrolovat permisions for api
+        print("Start test_buy_btc_by_limit_order")
+        response = self.exchange.create_order(symbol="BTC/USDT:USDT",
+                                              type="limit",
+                                              side="buy",
+                                              amount=0.001,
+                                              price=20000
+                                              )
+
+        print("Response: {}".format(response))
+        self.assertTrue("id" in response)
+
+        print("Finished test_buy_btc_by_limit_order")
+
+    def test_sell_btc_by_limit_order(self):  # TODO: @lucka zkontrolovat metodu
+        print("Start test_sell_btc_by_limit_order")
+        response = self.exchange.create_order(symbol="BTC/USDT:USDT",
+                                              type="limit",
+                                              side="sell",
+                                              amount=0.001,
+                                              price=30000
+                                              )
+        print("Response: {}".format(response))
+        self.assertTrue("id" in response)
+
+        print("Finished test_sell_btc_by_limit_order")
+
+    def test_get_positions(self):  # TODO: @lucka dokoncit metodu
+        print("Start test_get_open_positions")
+
+        print("Create small positions on BTC and ETH")
+        self.create_small_btc_long_position()
+        self.create_small_eth_long_position()
+
+        print("Get positions")
+        positions = self.exchange.fetch_positions()
+        print(positions)
+
+        self.assertEqual(len(positions["contracts"] == 0), 2)  # TODO: @lucka zjistit jak vyfilrovat otevrene pozice
+        self.assertEqual(positions["symbol"], "BTC/USDT:USDT")
+        self.assertEqual(positions["symbol"], "ETH/USDT:USDT")
+
+        print("Finished test_get_open_positions")
+
+    def create_small_btc_long_position(self, type_of_order="market", limit_price=None):  # TODO: @lucka zkontrolovat metodu
+        return self.exchange.create_order(symbol="BTC/USDT:USDT",
+                                          type=type_of_order,
+                                          side="buy",
+                                          amount=0.001,
+                                          price=limit_price
+                                          )
+
+    def create_small_eth_long_position(self, type_of_order="market", limit_price=None):  # TODO: @lucka zkontrolovat metodu
+        return self.exchange.create_order(symbol="ETH/USDT:USDT",
+                                          type=type_of_order,
+                                          side="buy",
+                                          amount=0.01,
+                                          price=limit_price,
+                                          )
+
 
     # TODO: @ Lucka doupravit a dokoncit tak aby bylo funkcni
     #def set_default_setting(self):
